63366981c35d6fec51f032a23b5c565828b164db
Manav Bhullar <manavbhullar341@gmail.com>
2026-03-27 15:49:39 +0530

    Remove backend files and configurations, including README, .gitignore, package.json, Prisma schema, and various modules

--- backend/src/modules/rideRequest/rideRequest.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createRideRequest } from './rideRequest.controller.js';
-
-const router = express.Router();
-
-router.post('/', createRideRequest);
-
-export default router;
